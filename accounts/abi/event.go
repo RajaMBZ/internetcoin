acde9e2b62d484d7265d755147be15bea6a6b204
RajaMBZ <34947728+RajaMBZ@users.noreply.github.com>
2018-11-30 23:25:06 +0500

    Update event.go

--- accounts/abi/event.go
+++ accounts/abi/event.go
@@ -20,8 +20,8 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/pirl/pirl/common"
-	"github.com/pirl/pirl/crypto"
+	"github.com/RajaMBZ/internetcoin/common"
+	"github.com/RajaMBZ/internetcoin/crypto"
 )
 
 // Event is an event potentially triggered by the EVM's LOG mechanism. The Event
